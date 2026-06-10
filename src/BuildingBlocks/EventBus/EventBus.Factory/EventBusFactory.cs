16a67d421eaed3834a978c6b6a3bad5be5564c1e
BurakDurmus1 <burakdurmus9605@gmail.com>
2026-06-10 16:30:52 +0300

    rabbitMq

---
+++ src/BuildingBlocks/EventBus/EventBus.Factory/EventBusFactory.cs
@@ -0,0 +1,22 @@
+﻿using EventBus.AzureServiceBus;
+using EventBus.Base;
+using EventBus.Base.Abstraction;
+using EventBus.RabbitMQ;
+using System;
+using static EventBus.Base.EventBusConfig;
+
+namespace EventBus.Factory
+{
+    public static class EventBusFactory
+    {
+        public static IEventBus Create(EventBusConfig config, IServiceProvider serviceProvider)
+        {
+           return config.EventBusType switch
+           {
+               EventBusTypes.RabbitMQ => new EventBusRabbitMQ(config, serviceProvider),
+               EventBusTypes.AzureServiceBus => new EventBusServiceBus(config, serviceProvider),
+               _ => throw new ArgumentException("Invalid event bus type")
+           };
+        }
+    }
+}
